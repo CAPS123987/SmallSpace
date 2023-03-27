1560afc268c164fa80c0bce2291a548b928ffc7a
CAPS123987 <hnilickakuba@gmail.com>
2023-03-27 16:31:06 +0200

    test Item

---
+++ src/main/java/me/CAPS123987/items/Items.java
@@ -0,0 +1,31 @@
+package me.CAPS123987.items;
+
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.core.attributes.MachineTier;
+import io.github.thebusybiscuit.slimefun4.core.attributes.MachineType;
+import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
+import io.github.thebusybiscuit.slimefun4.utils.LoreBuilder;
+import me.CAPS123987.smallspace.*;
+
+public class Items {
+	public static final ItemGroup smallSpace = new ItemGroup(new NamespacedKey(SmallSpace.getInstance(),
+	        "Small_Space"),
+	        new CustomItemStack(Material.TUFF, "&4Small_Space")
+	    );
+	public static final SlimefunItemStack TEST_ITEM = new SlimefunItemStack("TEST_ITEM",
+	        Material.DIAMOND_BLOCK,
+	        "TEST_ITEM",
+	        ""
+	    );
+	public static final ItemStack[] recipe_TEST_ITEM= {
+			null,null,null,
+			null,new ItemStack(Material.PINK_WOOL),null,
+			null,null,null
+	};
+}
--- src/main/java/me/CAPS123987/smallspace/SmallSpace.java
+++ src/main/java/me/CAPS123987/smallspace/SmallSpace.java
@@ -12,53 +12,25 @@ import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
+import me.CAPS123987.*;
+import me.CAPS123987.items.Items;
 
 public class SmallSpace extends JavaPlugin implements SlimefunAddon {
-
+	public static SmallSpace instance;
     @Override
     public void onEnable() {
         // Read something from your config.yml
+    	instance = this;
+    	
         Config cfg = new Config(this);
 
         if (cfg.getBoolean("options.auto-update")) {
             // You could start an Auto-Updater for example
         }
-
-        /*
-         * 1. Creating a new Category
-         * This Category will use the following ItemStack
-         */
-        ItemStack itemGroupItem = new CustomItemStack(Material.DIAMOND, "&4Addon Category", "", "&a> Click to open");
-
-        // Give your Category a unique id.
-        NamespacedKey itemGroupId = new NamespacedKey(this, "addon_category");
-        ItemGroup itemGroup = new ItemGroup(itemGroupId, itemGroupItem);
-
-        /*
-         * 2. Create a new SlimefunItemStack
-         * This class has many constructors, it is very important
-         * that you give each item a unique id.
-         */
-        SlimefunItemStack slimefunItem = new SlimefunItemStack("COOL_DIAMOND", Material.DIAMOND, "&4Cool Diamond", "&c+20% Coolness");
-
-        /*
-         * 3. Creating a Recipe
-         * The Recipe is an ItemStack Array with a length of 9.
-         * It represents a Shaped Recipe in a 3x3 crafting grid.
-         * The machine in which this recipe is crafted in is specified
-         * further down as the RecipeType.
-         */
-        ItemStack[] recipe = { new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD), null, new ItemStack(Material.DIAMOND), null, new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD) };
-
-        /*
-         * 4. Registering the Item
-         * Now you just have to register the item.
-         * RecipeType.ENHANCED_CRAFTING_TABLE refers to the machine in
-         * which this item is crafted in.
-         * Recipe Types from Slimefun itself will automatically add the recipe to that machine.
-         */
-        SlimefunItem item = new SlimefunItem(itemGroup, slimefunItem, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
-        item.register(this);
+        SlimefunItem sfItem = new SlimefunItem(Items.smallSpace, Items.TEST_ITEM, 
+        		RecipeType.ENHANCED_CRAFTING_TABLE, Items.recipe_TEST_ITEM);
+        sfItem.register(this);
+        
     }
 
     @Override
@@ -79,6 +51,9 @@ public class SmallSpace extends JavaPlugin implements SlimefunAddon {
          * If you are using your main class for this, simply return "this".
          */
         return this;
+    }
+    public static SmallSpace getInstance() {
+        return instance;
     }
 
 }
